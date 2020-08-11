04f37499ef3e9f99af2b865f10e851af8ca9e387
Colin Dixon <dixikinz@gmail.com>
2020-08-11 18:43:53 +0100

    Ball exists and randomly changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
@@ -20,10 +21,27 @@ class Ball extends StatefulWidget {
 }
 
 class _BallState extends State<Ball> {
+  int ballOpt = 1;
+
+  void changeOpt (){setState(() {
+                  ballOpt = Random().nextInt(5)+1;
+  },);}
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                changeOpt();
+              },
+              child: Image.asset('images/ball$ballOpt.png'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
